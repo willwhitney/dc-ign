61be4c27c60ce5c168b9c70aca00aca8e4557c3b
Tom White <tom@sixdozen.com>
2015-05-04 03:08:50 -0700

    monovariant_main: added lowerbound_test variable
    This variable is referenced in the codepath,
    so initializing with the return value from
    testf_MV() following the pattern in main.lua.

--- monovariant_main.lua
+++ monovariant_main.lua
@@ -239,7 +239,7 @@ while true do
 
 
   -- Compute the lowerbound of the test set and save it
-  testf_MV(false)
+  lowerbound_test = testf_MV(false)
   if true then
     if lowerbound_test_list then
       lowerbound_test_list = torch.cat(lowerbound_test_list
